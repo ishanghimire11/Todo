b69f21b52b2c4f17b4e8bdc2524f29bf93bd8030
ishanghimire11 <ishanuchiha11@gmail.com>
2023-01-05 13:46:46 +0545

    FIX: indexing and deleting

--- src/Todo.js
+++ src/Todo.js
@@ -2,46 +2,36 @@ import { useState } from "react"
 import "./todo.css"
 
 const Todo = (props) => {
-    const names=props.names
-    const index=props.index
+    const names = props.names
+    const index = props.index
     const d = new Date()
- 
+    const myindex = props.myindex
 
-    const dateonString= d.toString()
-    const [isActive,setActive]=useState(true)
+    const dateonString = d.toString()
+    const [isActive, setActive] = useState(true)
     const myDate = dateonString
-    
-    const handleDelete=props.handleDelete
-    
+
+
+    const handleDelete = props.handleDelete
+
     const handleChange = () => {
         setActive(!isActive)
 
     }
 
 
-    // const handleDate = () => {
-    //     if (!isActive){
-    //         let p = new Date()
-    //         let dated = p.toString()
-    //         setmyDate(dated)
-    //         console.log(myDate)
-    //     }
-    //     return true
-        
-    // }
-
-
     return (
         <>
             <li className="lists" >
-                <span id="index-number">{index}</span>
-                <input type="checkbox" id="checkbox" onChange={handleChange}  />
-                <p className="list-paragraph" style={{textTransform: "capitalize"}}>{ names }</p> 
+                <span id="index-number">{myindex + 1}</span>
+                <input type="checkbox" id="checkbox" onChange={handleChange} />
+                <p className="list-paragraph" style={{ textTransform: "capitalize" }}>{names}</p>
                 <span className={`${isActive ? "hidden" : "date-active"}`}>Completed on {myDate}</span>
-                <i className={`fa-solid ${isActive ? "fa-trash" : "fa-check"}`} onClick={()=> handleDelete(index,names)}></i>
+                <i className={`fa-solid ${isActive ? "fa-trash" : "hidden"}`} onClick={() => handleDelete(index, names)}></i>
+                <i className={`fa-solid ${isActive ? "hidden" : "fa-check"}`}></i>
             </li>
         </>
-    
+
     );
 }
 
